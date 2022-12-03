8e78b7a260e4073ff80c0405ddc03ab25dac94c1
Natalia Olmo <92785171+nataliaaolmo@users.noreply.github.com>
2022-12-03 18:46:40 +0100

    Delete Views.sql

--- Views.sql
+++
@@ -1,9 +0,0 @@
--- vista que contiene los restaurantes cuyo codigo postal es 41010
-CREATE OR REPLACE VIEW ViewZipCode AS
-	SELECT * FROM Restaurants WHERE zipCode = '41010';
--- SELECT COUNT(*) FROM ViewZipCode;
-
--- vista a partir de la anterior que me muestra los restaurante cuyo codigo postal es 41010 y tienen gastos de envio menores de 3.00 €	
-CREATE OR REPLACE VIEW ViewZipCodeShippingCosts AS 
-	SELECT * FROM ViewZipCode WHERE shippingCosts<3;
--- SELECT * FROM ViewZipCodeShippingCosts;
